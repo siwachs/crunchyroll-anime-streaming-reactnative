f2c9b4aef5bd1ae71cee5df4961ebc1c397507b5
siwachs <ssiwach.8888@gmail.com>
2024-02-11 09:44:56 +0530

    Add theme

---
+++ src/infrastructure/theme/colors.ts
@@ -0,0 +1,24 @@
+export const colors = {
+  ui: {
+    primary: "#262626",
+    secondary: "#757575",
+    tertiary: "#F1F1F1",
+    quaternary: "#FFFFFF",
+    disabled: "#DEDEDE",
+    error: "#D0421B",
+    success: "#138000",
+  },
+  bg: {
+    primary: "#000000",
+    secondary: "#23252b",
+    backdrop: "#23252bcc",
+  },
+  text: {
+    primary: "#262626",
+    secondary: "#757575",
+    disabled: "#9C9C9C",
+    inverse: "#FFFFFF",
+    error: "#D0421B",
+    success: "#138000",
+  },
+} as const;
