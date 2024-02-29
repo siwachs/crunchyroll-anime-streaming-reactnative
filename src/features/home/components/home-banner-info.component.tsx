38aaf9ff6e16ad79b0e8da479d95bc493d406322
siwachs <ssiwach.8888@gmail.com>
2024-02-29 19:57:16 +0530

    Home Banner

--- src/features/home/components/home-banner-info.component.tsx
+++ src/features/home/components/home-banner-info.component.tsx
@@ -6,21 +6,21 @@ import Touchable from "@/src/components/Utilities/touchable.component";
 const HomeBannerInfo = () => {
   return (
     <Touchable>
-      <View className="h-[62vh] w-full min-w-full overflow-hidden px-4">
-        <View className="h-[54vh] justify-end">
+      <View className="h-[64vh] w-full min-w-full overflow-hidden px-4">
+        <View className="h-[56vh] justify-end">
           <Text
             numberOfLines={2}
-            className="font-latoBold text-2xl uppercase text-white"
+            className="mb-2.5 font-latoBold text-2xl uppercase text-white"
           >
             Villainess Level 99: I May Be the Hidden Boss But I'm Not the Demon
             Lord
           </Text>
-          <Text className="font-lato text-sm capitalize text-slate-200">
+          <Text className="mb-2.5 font-lato text-sm capitalize text-slate-200">
             Dub | Sub
           </Text>
           <Text
             numberOfLines={3}
-            className="font-lato text-sm leading-[18px] text-white"
+            className="mb-4 font-lato text-sm leading-[18px] text-white"
           >
             This college kid wants nothing more than a quiet life. So when she’s
             reborn as Yumiella, the hidden villainess of an Otome RPG, she’s not
@@ -30,9 +30,9 @@ const HomeBannerInfo = () => {
             she’s the infamous Demon Lord. What future awaits her?
           </Text>
           <Touchable onPress={() => {}}>
-            <View className="h-10 w-40 flex-row items-center justify-center gap-x-1 bg-crunchyrollOrange">
+            <View className="w-44 flex-row justify-center bg-crunchyrollOrange py-2">
               <Feather name="play" size={25} color="black" />
-              <Text className="text-base font-semibold uppercase">
+              <Text className="ml-1 text-base font-semibold uppercase">
                 Watch Now
               </Text>
             </View>
